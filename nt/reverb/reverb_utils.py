f924a7d512dc21759ca6a7c28cfa27444cf78933
wilhelmk <wilhelmk@mail.uni-paderborn.de>
2015-10-22 18:46:44 +0200

    WIP: Implemented Cython code, but still with a bug. Moreover added a testcase which is not running because of the before mentioned bug.

--- nt/reverb/reverb_utils.py
+++ nt/reverb/reverb_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
-import nt.reverb.CalcRIR_Simple_C as RIR
+import nt.reverb.CalcRIR_Simple_C as tranVuRIR
+import random
 
 def generate_RIR(roomDimension, sourcePositions, sensorPositions, samplingRate,
                  filterLength, soundDecayTime, algorithm="TranVu",
@@ -7,8 +8,9 @@ def generate_RIR(roomDimension, sourcePositions, sensorPositions, samplingRate,
                  soundvelocity=343):
     """
     Generates a room impulse response.
-    :param roomDimension:
-    :param sourcePositions:
+    :param roomDimension: 3-floats-sequence; The room dimensions in meters
+    :param sourcePositions: List of 3-floats-lists containing the source
+    position coordinates in meters
     :param sensorPositions:
     :param samplingRate:
     :param filterLength:
@@ -18,8 +20,18 @@ def generate_RIR(roomDimension, sourcePositions, sensorPositions, samplingRate,
     :param sensorDirectivity:
     :param soundvelocity:
     :return:
+
+    Example:
+    >>> roomDim = (10,10,4)
+    >>> sources = ((1,1,2),)
+    >>> mics = ((2,3,2),(9,9,2))
+    >>> sampleRate = 16000
+    >>> filterLength = 2**13
+    >>> T60=0.3
+    >>> pyRIR = generate_RIR(roomDim,sources,mics,sampleRate, filterLength,T60)
     """
 
+    # These are lists of possible picks
     algorithmList = ("TranVu","Habets","Lehmann","LehmannFast","AllenBerkley")
     directivityList = {"omnidirectional":1,"subcardioid":0.75,"cardioid":0.5,
                        "hypercardioid":0.25,"bidirectional":0}
@@ -36,12 +48,12 @@ def generate_RIR(roomDimension, sourcePositions, sensorPositions, samplingRate,
     if not len(roomDimension)==3:
         raise Exception("RoomDimensions needs 3 positive numbers!")
     if not (len(sourcePositions[s]) == 3 for s in range(numSources)) or \
-            not all(isInsideRoom(roomDimension,[s,t,u]) for s,t,u \
-            in sourcePositions):
+            not all(isInsideRoom(roomDimension,[u,v,w]) for u,v,w \
+                    in sourcePositions):
         raise Exception("Source positions aren't lists of positive 3-element-"
                         "lists or inside room dimensions!")
     if not (len(sensorPositions[s]) == 3 for s in range(numSensors)) or \
-            not all(isInsideRoom(roomDimension,[s,t,u]) for s,t,u \
+            not (all(isInsideRoom(roomDimension,[s,t,u])) for s,t,u \
             in sensorPositions):
         raise Exception("Sensor positions aren't lists of positive 3-element-"
                         "lists or inside room dimensions!")
@@ -56,7 +68,7 @@ def generate_RIR(roomDimension, sourcePositions, sensorPositions, samplingRate,
                                                "one of the following: \n"+
                                                 algorithmList)
     if not any(sensorDirectivity == key for key in directivityList):
-        raise Exception("sensor directivity " + sensorDirectivity + " unknown!")
+        raise Exception("sensor directivity "+sensorDirectivity+" unknown!")
     if not np.isscalar(soundvelocity):
         raise Exception("sound velocity isn't scalar!")
 
@@ -71,13 +83,19 @@ def generate_RIR(roomDimension, sourcePositions, sensorPositions, samplingRate,
 
     alpha = directivityList[sensorDirectivity]
 
-    rir = np.empty((filterLength,numSensors,numSources))
+    rir = np.zeros((filterLength,numSensors,numSources))
 
-    # todo: Unterscheide zwischen allen algorithmen
+    # todo: Unterscheide zwischen allen Algorithmen
     # TranVU method
     noiseFloor = -60
-    rir = RIR.calc()
-
+    rir = tranVuRIR.calc(np.asarray(roomDimension,dtype=np.float32),
+                   np.asarray(sourcePositions,dtype=np.float32),
+                   np.asarray(sensorPositions,dtype=np.float32),
+                   samplingRate,
+                   filterLength,soundDecayTime*1000,noiseFloor,
+                   np.asarray(sensorOrientations, dtype=np.float32 ),
+                   alpha,soundvelocity)
+    return rir
 
 
 
@@ -89,5 +107,32 @@ def isInsideRoom(roomDim,x):
     :param x: 3-object-sequence. Denotes the point to verify.
     :return: True for x being inside the room dimensions and False otherwise.
     """
-    positive = all(elem > 0 for elem in x)
-    return positive & all(elem > 0 for elem in np.subtract(roomDim,x))
+    positive = all([elem > 0 for elem in x]) # all elements shall be greater 0
+    return positive and np.all(np.subtract(roomDim,x))
+
+def generateRandomSourcesAndSensors(roomDim,numSources,numSensors):
+    """
+    Returns two lists with random sources and sensors
+    within the room dimensions
+    :param roomDim: 1x3 list; room dimensions in meters e.g. [9,7,3]
+    :param numSources: Integer; Number of desired source positions e.g. 3
+    :param numSensors: Integer; Number of desired sensor positions e.g. 1
+    :return:srcList,micList: Each is a list of 3-element-lists denoting
+            the positions' coordinates
+    """
+    srcList = []
+    micList = []
+    # todo: avoid sensors/sources in the room's center
+    for s in range(numSources):
+        x = random.uniform(10**-3,roomDim[0])
+        y = random.uniform(10**-3,roomDim[1])
+        z = random.uniform(10**-3,roomDim[2])
+        srcList.append([x,y,z])
+    for m in range(numSensors):
+        x = random.uniform(10**-3,roomDim[0])
+        y = random.uniform(10**-3,roomDim[1])
+        z = random.uniform(10**-3,roomDim[2])
+        micList.append([x,y,z])
+    return srcList,micList
+
+
