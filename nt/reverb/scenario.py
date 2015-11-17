eea669c694dac75442d376554914c5bf0b4bef95
wilhelmk <wilhelmk@mail.uni-paderborn.de>
2015-11-17 11:20:18 +0100

    Added testcases comparing different directivities and sensor orientations. Moved environmental methods to scenario.py.

--- nt/reverb/scenario.py
+++ nt/reverb/scenario.py
@@ -12,7 +12,7 @@ Helps to quickly create source and sensor positions. Try it with the following c
 import numpy
 import matplotlib.pyplot as plt
 from operator import add
-
+import random
 
 def generate_sensor_positions(shape='cube', center=numpy.zeros((3, 1)), scale=0.01):
     """
@@ -55,7 +55,7 @@ def generate_random_source_positions(center=numpy.zeros((3, 1)), n=1, distance_i
     Samples are drawn from a uniform distribution on a hollow sphere with
     inner and outer radius according to distance_interval.
 
-    The idea is to sample from a angular centric Gaussian distribution.
+    The idea is to sample from an angular centric Gaussian distribution.
     """
     x = numpy.random.normal(size=(3, n))
     if dims == 2:
@@ -118,6 +118,42 @@ def generate_deterministic_source_positions(center=numpy.zeros((3,1)),
     pos += radius*z
     return pos
 
+def isInsideRoom(roomDim,x):
+    """
+    Treats x as 3-dim vector and determines whether it's inside the
+     room dimensions.
+    :param roomDim: 3-object-sequence. Denotes the room dimensions.
+    :param x: 3-object-sequence. Denotes the point to verify.
+    :return: True for x being inside the room dimensions and False otherwise.
+    """
+    positive = all([elem > 0 for elem in x]) # all elements shall be greater 0
+    return positive and numpy.all(numpy.subtract(roomDim,x))
+
+
+def generate_uniformly_random_sources_and_sensors(roomDim,numSources,numSensors):
+    """
+    Returns two lists with random sources and sensors
+    within the room dimensions
+    :param roomDim: 1x3 list; room dimensions in meters e.g. [9,7,3]
+    :param numSources: Integer; Number of desired source positions e.g. 3
+    :param numSensors: Integer; Number of desired sensor positions e.g. 1
+    :return:srcList,micList: Each is a list of 3-element-lists denoting
+            the positions' coordinates
+    """
+    srcList = []
+    micList = []
+    # todo: avoid sensors/sources in the room's center
+    for s in range(numSources):
+        x = random.uniform(10**-3,roomDim[0])
+        y = random.uniform(10**-3,roomDim[1])
+        z = random.uniform(10**-3,roomDim[2])
+        srcList.append([x,y,z])
+    for m in range(numSensors):
+        x = random.uniform(10**-3,roomDim[0])
+        y = random.uniform(10**-3,roomDim[1])
+        z = random.uniform(10**-3,roomDim[2])
+        micList.append([x,y,z])
+    return srcList,micList
 
 def plot(src=None, mic=None, room=None):
     fig = plt.figure(figsize=(8, 3.5))
