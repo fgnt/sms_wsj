041d8b3076634bdd2605c767c1797e237507c924
Lukas Drude <mail@lukas-drude.de>
2016-02-15 18:51:47 +0100

    Merged all steering_vector and tdoa related functions in reverb_utils.py and beamform_utils.py

--- nt/reverb/reverb_utils.py
+++ nt/reverb/reverb_utils.py
@@ -3,11 +3,12 @@ Offers methods for calculating room impulse responses and convolutions of these
 with audio signals.
 """
 
-import numpy,scipy
-import nt.reverb.CalcRIR_Simple_C as tranVuRIR
-#import nt.reverb.Habets_RIR_C as HabetsRIR
-import nt.reverb.scenario as scenario
+import numpy
 from scipy import signal
+
+import nt.reverb.CalcRIR_Simple_C as tranVuRIR
+import nt.reverb.scenario as scenario
+
 
 def generate_RIR(roomDimension, sourcePositions, sensorPositions, samplingRate,
                  filterLength, soundDecayTime, algorithm="TranVu",
@@ -50,9 +51,11 @@ def generate_RIR(roomDimension, sourcePositions, sensorPositions, samplingRate,
     """
 
     # These are lists of possible picks
-    algorithmList = ("TranVu","Habets","Lehmann","LehmannFast","AllenBerkley")
-    directivityList = {"omnidirectional":1,"subcardioid":0.75,"cardioid":0.5,
-                       "hypercardioid":0.25,"bidirectional":0}
+    algorithmList = (
+    "TranVu", "Habets", "Lehmann", "LehmannFast", "AllenBerkley")
+    directivityList = {"omnidirectional": 1, "subcardioid": 0.75,
+                       "cardioid": 0.5,
+                       "hypercardioid": 0.25, "bidirectional": 0}
 
     # get number of sensors and sources
     try:
@@ -63,30 +66,32 @@ def generate_RIR(roomDimension, sourcePositions, sensorPositions, samplingRate,
               "len() on them.")
 
     # verify input for correct datatypes and values
-    if not len(roomDimension)==3:
+    if not len(roomDimension) == 3:
         raise Exception("RoomDimensions needs 3 positive numbers!")
     if not (len(sourcePositions[s]) == 3 for s in range(numSources)) or \
-            not all(scenario.isInsideRoom(roomDimension,[u,v,w]) for u,v,w \
-                    in sourcePositions):
+            not all(
+                scenario.is_inside_room(roomDimension, [u, v, w]) for u, v, w \
+                in sourcePositions):
         raise Exception("Source positions aren't lists of positive 3-element-"
                         "lists or inside room dimensions!")
     if not (len(sensorPositions[s]) == 3 for s in range(numSensors)) or \
-            not (all(scenario.isInsideRoom(roomDimension,[s,t,u])) for s,t,u \
-            in sensorPositions):
+            not (all(scenario.is_inside_room(roomDimension, [s, t, u])) for
+                 s, t, u \
+                 in sensorPositions):
         raise Exception("Sensor positions aren't lists of positive 3-element-"
                         "lists or inside room dimensions!")
     if not numpy.isscalar(samplingRate):
         raise Exception("sampling rate isn't scalar!")
     if not numpy.isscalar(filterLength):
         raise Exception("Filter length isn't scalar!")
-    if type(soundDecayTime)==str:
+    if type(soundDecayTime) == str:
         raise Exception("sound decay time should be numeric!")
     if not any(algorithm == s for s in algorithmList):
-        raise Exception("algorithm "+algorithm+" is unknown! Please choose"
-                                               "one of the following: \n"+
-                                                algorithmList)
+        raise Exception("algorithm " + algorithm + " is unknown! Please choose"
+                                                   "one of the following: \n" +
+                        algorithmList)
     if not any(sensorDirectivity == key for key in directivityList):
-        raise Exception("sensor directivity "+sensorDirectivity+" unknown!")
+        raise Exception("sensor directivity " + sensorDirectivity + " unknown!")
     if not numpy.isscalar(soundvelocity):
         raise Exception("sound velocity isn't scalar!")
 
@@ -94,58 +99,34 @@ def generate_RIR(roomDimension, sourcePositions, sensorPositions, samplingRate,
     if not sensorOrientations:
         sensorOrientations = []
         for x in range(numSensors):
-            sensorOrientations.append((0,0))
+            sensorOrientations.append((0, 0))
 
     # todo: Fall 'Lehmann' und 'omnidirectional' ausschließen!
     # todo: Fall 'LehmannFast' und sound velocity != 343 ausschließen!
 
     alpha = directivityList[sensorDirectivity]
 
-    rir = numpy.zeros((filterLength,numSensors,numSources))
+    rir = numpy.zeros((filterLength, numSensors, numSources))
 
     # todo: Mehr Algorithmen in Betracht ziehen
     if algorithm == "TranVu":
         # TranVU method
         noiseFloor = -60
-        rir = tranVuRIR.calc(numpy.asarray(roomDimension,dtype=numpy.float64),
-                       numpy.asarray(sourcePositions,dtype=numpy.float64),
-                       numpy.asarray(sensorPositions,dtype=numpy.float64),
-                       samplingRate,
-                       filterLength,soundDecayTime*1000,noiseFloor,
-                       numpy.asarray(sensorOrientations, dtype=numpy.float64 ),
-                       alpha,soundvelocity)
+        rir = tranVuRIR.calc(numpy.asarray(roomDimension, dtype=numpy.float64),
+                             numpy.asarray(sourcePositions,
+                                           dtype=numpy.float64),
+                             numpy.asarray(sensorPositions,
+                                           dtype=numpy.float64),
+                             samplingRate,
+                             filterLength, soundDecayTime * 1000, noiseFloor,
+                             numpy.asarray(sensorOrientations,
+                                           dtype=numpy.float64),
+                             alpha, soundvelocity)
     else:
-        raise NotImplementedError("The chosen algorithm is not implemented yet.")
+        raise NotImplementedError(
+            "The chosen algorithm is not implemented yet.")
     return rir
 
-def nearfield_time_of_flight(source_positions, sensor_positions, sound_velocity=343):
-    """ Calculates exact time of flight in seconds without farfield assumption.
-
-    :param source_positions: Array of 3D source position column vectors.
-    :param sensor_positions: Array of 3D sensor position column vectors.
-    :param sound_velocity: Speed of sound in m/s.
-    :return: Time of flight in s.
-    """
-    # TODO: Check, if this works for any number of sources and sensors.
-    difference = source_positions[:, :, numpy.newaxis] - sensor_positions[:, numpy.newaxis, :]
-    difference = numpy.linalg.norm(difference, axis=0)
-    return difference / sound_velocity
-
-
-def steering_vector(time_of_flight, frequency):
-    """ Calculates a steering vector.
-
-    Keep in mind, that many steering vectors describe the same.
-
-    :param time_of_flight: Time of flight in s.
-    :param frequency: Vector of center frequencies as created
-        by `get_stft_center_frequencies()`.
-    :return:
-    """
-    # TODO: Check, if this works for any number of sources and sensors.
-    return numpy.exp(-2j * numpy.pi
-                     * frequency[numpy.newaxis, numpy.newaxis, :]
-                     * time_of_flight[:, :, numpy.newaxis])
 
 def fft_convolve(x, impulse_response):
     """
@@ -163,25 +144,31 @@ def fft_convolve(x, impulse_response):
         [number_sensors x number_sources x signal_length - numpy matrix]
         The convoluted signal for every sensor and each source
     """
-    # Get number of sources and sensors
-    num_sensors = impulse_response.shape[1]
-    num_sources = impulse_response.shape[2]
-    if not num_sources == x.shape[0]:
-        raise Exception("Number audio signals ("+str(x.shape[0])+") does "
-                        "not match source positions ("+str(num_sources)+") "
-                        "in given impulse response!")
-    convolved_signal = numpy.zeros([num_sensors,
-                                    num_sources,
-                                    x.shape[1]+len(impulse_response)-1])
-    # fftconvolve for every sensor and source
-    for i in range(num_sensors):
-        for j in range(num_sources):
-            convolved_signal[i,j,:] = signal.fftconvolve(x[j,:],
-                                                         impulse_response[:,i,j])
+    _, sensors, sources = impulse_response.shape
+
+    if not sources == x.shape[0]:
+        raise Exception(
+            "Number audio signals (" +
+            str(x.shape[0]) +
+            ") does not match source positions (" +
+            str(sources) +
+            ") in given impulse response!"
+        )
+    convolved_signal = numpy.zeros(
+        [sensors, sources, x.shape[1] + len(impulse_response) - 1]
+    )
+
+    for i in range(sensors):
+        for j in range(sources):
+            convolved_signal[i, j, :] = signal.fftconvolve(
+                x[j, :],
+                impulse_response[:, i, j]
+            )
 
     return convolved_signal
 
-def time_convolve(x,impulse_response):
+
+def time_convolve(x, impulse_response):
     """
     Takes audio signals and the impulse responses according to their position
     and returns the convolution. The number of audio signals in x are required
@@ -197,25 +184,25 @@ def time_convolve(x,impulse_response):
         [number_sensors x number_sources x signal_length - numpy matrix]
         The convoluted signal for every sensor and each source
     """
-    # Get number of sources and sensors
-    num_sensors = impulse_response.shape[1]
-    num_sources = impulse_response.shape[2]
-    if not num_sources == x.shape[0]:
-        raise Exception("Number audio signals ("+str(x.shape[0])+") does "
-                        "not match source positions ("+str(num_sources)+") "
-                        "in given impulse response!")
-    convolved_signal = numpy.zeros([num_sensors,
-                                    num_sources,
-                                    x.shape[1]+len(impulse_response)-1])
-    # convolve for every sensor and source
-    for i in range(num_sensors):
-        for j in range(num_sources):
-            convolved_signal[i,j,:] = numpy.convolve(x[j,:],
-                                                     impulse_response[:,i,j])
+    _, sensors, sources = impulse_response.shape
+
+    if not sources == x.shape[0]:
+        raise Exception(
+            "Number audio signals (" +
+            str(x.shape[0]) +
+            ") does not match source positions (" +
+            str(sources) +
+            ") in given impulse response!"
+        )
+    convolved_signal = numpy.zeros(
+        [sensors, sources, x.shape[1] + len(impulse_response) - 1]
+    )
+
+    for i in range(sensors):
+        for j in range(sources):
+            convolved_signal[i, j, :] = numpy.convolve(
+                x[j, :],
+                impulse_response[:, i, j]
+            )
 
     return convolved_signal
-
-#if __name__ == "__main__":
-#    import doctest
-#    doctest.testmod()
-
