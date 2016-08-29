c194b58e5011218f3f8d2d523d71f56543851c26
Lukas Drude <mail@lukas-drude.de>
2016-08-29 15:36:07 +0200

    Braking changes to RIR generator. Some test cases still refer to old code and need reconstruction.

--- nt/reverb/reverb_utils.py
+++ nt/reverb/reverb_utils.py
@@ -10,48 +10,58 @@ import scipy
 import nt.reverb.CalcRIR_Simple_C as tranVuRIR
 import nt.reverb.scenario as scenario
 
+eps = 1e-60
+window_length = 256
+
 
 def generate_rir(
-        room,
-        sources,
-        sensors,
+        room_dimensions,
+        source_positions,
+        sensor_positions,
         sound_decay_time,
-        sampling_rate=16000,
-        filter_length=2**13,
+        sample_rate=16000,
+        filter_length=2 ** 13,
         sensor_orientations=None,
         sensor_directivity=None,
         sound_velocity=343,
         algorithm=None
 ):
-    """ Wrapper of Wilhelm's RIR generator with sane defaults.
+    """ Wrapper for different RIR generators. Will replace generate_RIR().
 
     Args:
-        room: Numpy array with shape (3, 1)
+        room_dimensions: Numpy array with shape (3, 1)
             which holds coordinates x, y and z.
-        sources: Numpy array with shape (3, number_of_sources)
+        source_positions: Numpy array with shape (3, number_of_sources)
             which holds coordinates x, y and z in each column.
-        sensors: Numpy array with shape (3, number_of_sensors)
+        sensor_positions: Numpy array with shape (3, number_of_sensors)
             which holds coordinates x, y and z in each column.
         sound_decay_time: Reverberation time in seconds.
-        sampling_rate: Sampling rate in Hertz.
+        sample_rate: Sampling rate in Hertz.
         filter_length: Filter length, typically 2**13.
             Longer huge reverberation times.
         sensor_orientations: Numpy array with shape (2, 1)
             which holds azimuth and elevation angle in each column.
         sensor_directivity: String determining directivity for all sensors.
         sound_velocity: Set to 343 m/s.
-        algorithm: The only implemented algorithm is 'TranVu'.
+        algorithm: The only implemented algorithm are
+            'tran_vu_cython', 'tran_vu_python', 'tran_vu_python_loopy'.
 
     Returns: Numpy array of room impulse respones with
         shape (number_of_sources, number_of_sensors, filter_length).
     """
 
-    assert room.shape == (3, 1)
-    assert sources.shape[0] == 3
-    assert sensors.shape[0] == 3
+    assert room_dimensions.shape == (3, 1)
+    assert source_positions.shape[0] == 3
+    assert sensor_positions.shape[0] == 3
 
-    number_of_sources = sources.shape[1]
-    number_of_sensors = sensors.shape[1]
+    number_of_sources = source_positions.shape[1]
+    number_of_sensors = sensor_positions.shape[1]
+
+    if algorithm is None:
+        algorithm = 'tran_vu_cython'
+    assert algorithm in [
+        'tran_vu_cython', 'tran_vu_python', 'tran_vu_python_loopy'
+    ], 'Unknown algorithm {}'.format(algorithm)
 
     if sensor_orientations is None:
         sensor_orientations = np.zeros((2, number_of_sources))
@@ -59,21 +69,58 @@ def generate_rir(
     if sensor_directivity is None:
         sensor_directivity = 'omnidirectional'
 
-    if algorithm is None:
-        algorithm = 'TranVu'
+    if algorithm == 'tran_vu_cython':
+        assert (
+            np.all(sensor_orientations == np.zeros((2, number_of_sources))) and
+            sensor_directivity == 'omnidirectional'
+        ), 'Directional sensors is wrongly implemented in the Cython code.'
 
-    rir = generate_RIR(
-        roomDimension=list(room[:, 0]),
-        sourcePositions=sources.T,
-        sensorPositions=sensors.T,
-        samplingRate=sampling_rate,
-        filterLength=filter_length,
-        soundDecayTime=sound_decay_time,
-        algorithm=algorithm,
-        sensorOrientations=sensor_orientations,
-        sensorDirectivity=sensor_directivity,
-        soundvelocity=sound_velocity
-    ).transpose((2, 1, 0))
+        rir = generate_RIR(
+            roomDimension=list(room_dimensions[:, 0]),
+            sourcePositions=source_positions.T,
+            sensorPositions=sensor_positions.T,
+            samplingRate=sample_rate,
+            filterLength=filter_length,
+            soundDecayTime=sound_decay_time,
+            algorithm='TranVu',
+            sensorOrientations=sensor_orientations,
+            sensorDirectivity=sensor_directivity,
+            soundvelocity=sound_velocity
+        ).transpose((2, 1, 0))
+
+    elif algorithm == 'tran_vu_python':
+        assert (
+            np.all(sensor_orientations == np.zeros((2, number_of_sources))) and
+            sensor_directivity == 'omnidirectional'
+        ), 'Directional sensors are not supported with this algorithm.'
+
+        rir = _generate_rir_tran_vu_python(
+            room_dimensions=room_dimensions,
+            source_positions=source_positions,
+            sensor_positions=sensor_positions,
+            sound_decay_time=sound_decay_time,
+            filter_length=filter_length,
+            sampling_rate=sample_rate
+        )
+
+    elif algorithm == 'tran_vu_python_loopy':
+        assert (
+            np.all(sensor_orientations == np.zeros((2, number_of_sources))) and
+            sensor_directivity == 'omnidirectional'
+        ), 'Directional sensors are not supported with this algorithm.'
+
+        rir = _generate_rir_tran_vu_python_loopy(
+            room_dimensions=room_dimensions,
+            source_positions=source_positions,
+            sensor_positions=sensor_positions,
+            sound_decay_time=sound_decay_time,
+            filter_length=filter_length,
+            sampling_rate=sample_rate
+        )
+    else:
+        raise NotImplementedError(
+            'Algorithm "{}" is unknown.'.format(algorithm)
+        )
 
     assert rir.shape[0] == number_of_sources
     assert rir.shape[1] == number_of_sensors
@@ -124,7 +171,7 @@ def generate_RIR(roomDimension, sourcePositions, sensorPositions, samplingRate,
 
     # These are lists of possible picks
     algorithmList = (
-    "TranVu", "Habets", "Lehmann", "LehmannFast", "AllenBerkley")
+        "TranVu", "Habets", "Lehmann", "LehmannFast", "AllenBerkley")
     directivityList = {"omnidirectional": 1, "subcardioid": 0.75,
                        "cardioid": 0.5,
                        "hypercardioid": 0.25, "bidirectional": 0}
@@ -197,6 +244,207 @@ def generate_RIR(roomDimension, sourcePositions, sensorPositions, samplingRate,
     else:
         raise NotImplementedError(
             "The chosen algorithm is not implemented yet.")
+    return rir
+
+
+def blackman_harris_window(x):
+    # Can not be replaced by from scipy.signal import blackmanharris.
+    a0 = 0.35875
+    a1 = 0.48829
+    a2 = 0.14128
+    a3 = 0.01168
+    x = np.pi * (x - window_length / 2) / window_length
+    x = a0 - a1 * np.cos(2.0 * x) + a2 * np.cos(4.0 * x) - a3 * np.cos(6.0 * x)
+    return np.maximum(x, 0)
+
+
+def _generate_rir_tran_vu_python_loopy(
+        room_dimensions,
+        source_positions,
+        sensor_positions,
+        sound_decay_time,
+        filter_length,
+        sampling_rate,
+        noise_threshold=-60,
+        sound_velocity=343,
+        dtype=np.float64
+):
+    air_coefficient = 0.9991
+
+    norm_cut_off = 0.95
+    samples_per_meter = sampling_rate / sound_velocity
+
+    sources = source_positions.shape[1]
+    sensors = sensor_positions.shape[1]
+
+    if sound_decay_time > 0:
+        room_surface = 2 * (
+            room_dimensions[0] * room_dimensions[1] +
+            room_dimensions[1] * room_dimensions[2] +
+            room_dimensions[2] * room_dimensions[0]
+        )
+        room_volume = np.prod(room_dimensions)
+
+        # Unknown parameter
+        alpha = 1 - np.exp(
+            (-24 * np.log(10) * room_volume) /
+            (sound_velocity * sound_decay_time * room_surface)
+        )
+        reflection_coefficient = -np.sqrt(1 - alpha)
+
+        image_order = int(np.maximum(
+            10,
+            np.ceil(noise_threshold / 10 / np.log10(1 - alpha) / 3)
+        ))
+    else:
+        reflection_coefficient = 0.0
+        image_order = 0
+
+    rir = np.zeros((sources, sensors, filter_length), dtype=dtype)
+
+    for s in range(sources):
+
+        for z in range(-image_order, image_order + 1):
+
+            image = np.zeros((3,), dtype=dtype)
+            if z % 2 == 0:
+                image[2] = z * room_dimensions[2] + source_positions[2, s]
+            else:
+                image[2] = (z + 1) * room_dimensions[2] - source_positions[2, s]
+
+            for x in range(-image_order, image_order + 1):
+
+                if x % 2 == 0:
+                    image[0] = x * room_dimensions[0] + source_positions[0, s]
+                else:
+                    image[0] = (x + 1) * room_dimensions[0] - source_positions[
+                        0, s]
+
+                for y in range(-image_order, image_order + 1):
+                    if y % 2 == 0:
+                        image[1] = y * room_dimensions[1] + source_positions[
+                            1, s]
+                    else:
+                        image[1] = (y + 1) * room_dimensions[1] - \
+                                   source_positions[1, s]
+
+                    for m in range(sensors):
+                        difference = image - sensor_positions[:, m]
+                        distance = np.linalg.norm(difference)
+                        attenuation = reflection_coefficient ** (
+                            np.abs(x) + np.abs(y) + np.abs(z)
+                        ) * air_coefficient ** distance / (1 + distance)
+
+                        distance_in_samples = distance * samples_per_meter
+                        int_delay = int(distance_in_samples + 0.5)
+                        fractional_delay = distance_in_samples - int_delay
+                        count = - fractional_delay - window_length / 2
+
+                        for t in range(window_length):
+                            if int_delay + t < filter_length:
+                                win_si = np.pi * norm_cut_off * count
+                                if np.abs(win_si) < eps:
+                                    # 4th order Taylor approximation of windowed sinc()
+                                    x2 = win_si ** 2
+                                    win_si = norm_cut_off * blackman_harris_window(
+                                        count) * (
+                                                 1.0 - x2 / 6.0 + x2 ** 2 / 120)
+                                else:
+                                    # Direct computation of windowed sinc()
+                                    # print(norm_cut_off, blackman_harris_window(count), np.sin(win_si), win_si)
+                                    win_si = norm_cut_off * blackman_harris_window(
+                                        count) * np.sin(win_si) / win_si
+                                a = int_delay + t
+                                # print(count, attenuation, win_si)
+                                rir[s, m, a] += attenuation * win_si
+                                count += 1
+                            else:
+                                break
+    return rir
+
+
+def _generate_rir_tran_vu_python(
+        room_dimensions,
+        source_positions,
+        sensor_positions,
+        sound_decay_time,
+        filter_length,
+        sampling_rate,
+        noise_threshold=-60,
+        sound_velocity=343,
+        dtype=np.float64
+):
+    air_coefficient = 0.9991
+
+    norm_cut_off = 0.95
+    samples_per_meter = sampling_rate / sound_velocity
+
+    sources = source_positions.shape[1]
+    sensors = sensor_positions.shape[1]
+
+    if sound_decay_time > 0:
+        room_surface = 2 * (
+            room_dimensions[0] * room_dimensions[1] +
+            room_dimensions[1] * room_dimensions[2] +
+            room_dimensions[2] * room_dimensions[0]
+        )
+        room_volume = np.prod(room_dimensions)
+
+        # Unknown parameter
+        alpha = 1 - np.exp(
+            (-24 * np.log(10) * room_volume) /
+            (sound_velocity * sound_decay_time * room_surface)
+        )
+        reflection_coefficient = -np.sqrt(1 - alpha)
+
+        image_order = int(np.maximum(
+            10,
+            np.ceil(noise_threshold / 10 / np.log10(1 - alpha) / 3)
+        ))
+    else:
+        reflection_coefficient = 0.0
+        image_order = 0
+
+    image = np.zeros((3,), dtype=dtype)
+    rir = np.zeros((sources, sensors, filter_length), dtype=dtype)
+
+    for s in range(sources):
+        for z in range(-image_order, image_order + 1):
+            image[2] = (z + (z % 2)) * room_dimensions[2] + \
+                       (-1) ** (z % 2) * source_positions[2, s]
+
+            for x in range(-image_order, image_order + 1):
+                image[0] = (x + (x % 2)) * room_dimensions[0] + \
+                           (-1) ** (x % 2) * source_positions[0, s]
+
+                for y in range(-image_order, image_order + 1):
+                    image[1] = (y + (y % 2)) * room_dimensions[1] + \
+                               (-1) ** (y % 2) * source_positions[1, s]
+
+                    for m in range(sensors):
+                        difference = image - sensor_positions[:, m]
+                        distance = np.linalg.norm(difference)
+                        attenuation = reflection_coefficient ** (
+                            np.abs(x) + np.abs(y) + np.abs(z)
+                        ) * air_coefficient ** distance / (1 + distance)
+
+                        distance_in_samples = distance * samples_per_meter
+                        int_delay = int(distance_in_samples + 0.5)
+                        fractional_delay = distance_in_samples - int_delay
+                        count = - fractional_delay - window_length / 2
+
+                        for t in range(window_length):
+                            if int_delay + t < filter_length:
+                                win_si = np.pi * norm_cut_off * count
+                                win_si = np.where(win_si == 0, 1.0e-20, win_si)
+                                win_si = norm_cut_off * blackman_harris_window(
+                                    count) * np.sin(win_si) / win_si
+                                a = int_delay + t
+                                rir[s, m, a] += attenuation * win_si
+                                count += 1
+                            else:
+                                break
+
     return rir
 
 
