6a74b2538fdc9d3d371533ab2383f95139226404
Lukas Drude <ldrude@mail.uni-paderborn.de>
2017-04-04 14:14:19 +0200

    Generate Chime 3 microphone array

---
+++ nt/math/rotation.py
@@ -0,0 +1,33 @@
+"""Contains functions regarding 3D rotation matrices. Used in `scenario.py`."""
+import numpy as np
+
+
+def rot_x(alpha):
+    """Returns rotation matrix."""
+    return np.asarray(
+        [
+            [1, 0, 0],
+            [0, np.cos(alpha), -np.sin(alpha)],
+            [0, np.sin(alpha), np.cos(alpha)]
+        ]
+    )
+
+def rot_y(alpha):
+    """Returns rotation matrix."""
+    return np.asarray(
+        [
+            [np.cos(alpha), 0, np.sin(alpha)],
+            [0, 1, 0],
+            [-np.sin(alpha), 0, np.cos(alpha)]
+        ]
+    )
+
+def rot_z(alpha):
+    """Returns rotation matrix."""
+    return np.asarray(
+        [
+            [np.cos(alpha), -np.sin(alpha), 0],
+            [np.sin(alpha), np.cos(alpha), 0],
+            [0, 0, 1]
+        ]
+    )
