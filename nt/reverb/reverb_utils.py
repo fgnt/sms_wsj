b0d3dcd50ce7c5106918573af8a2b157ef50eefb
Lukas Drude <mail@lukas-drude.de>
2018-08-06 11:19:35 +0200

    Script to write transcription file from word transcription

--- nt/reverb/reverb_utils.py
+++ nt/reverb/reverb_utils.py
@@ -576,6 +576,36 @@ def convolve(signal, impulse_response, truncate=False):
             )
     return x[..., :samples] if truncate else x
 
+
+def get_rir_start_sample(h, level_ratio=1e-1):
+    """Finds start sample in a room impulse response.
+
+    Selects that index as start sample where the first time
+    a value larger than `level_ratio * max_abs_value`
+    occurs.
+
+    If you intend to use this heuristic, test it on simulated and real RIR
+    first. This heuristic is developed on MIRD database RIRs and on some
+    simulated RIRs but may not be appropriate for your database.
+
+    If you want to use it to shorten impulse responses, keep the initial part
+    of the room impulse response intact and just set the tail to zero.
+
+    Params:
+        h: Room impulse response with Shape (num_samples,)
+        level_ratio: Ratio between start value and max value.
+    """
+    assert h.ndim == 1, h.shape
+    abs_h = np.abs(h)
+    max_index = np.argmax(abs_h)
+    max_abs_value = abs_h[max_index]
+    larger_than_threshold = abs_h[:max_index] > level_ratio * max_abs_value
+
+    # Finds first occurrence of max
+    rir_start_sample = np.argmax(larger_than_threshold)
+    return rir_start_sample
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
