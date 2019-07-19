998a0d8c669a768fdeffe2bbae75ab77ab8a27ee
jensheit <heitkaemper@nt.upb.de>
2019-07-19 15:49:38 +0200

    adds inital version of wsj tri3 model training

--- sms_wsj/train_baseline_asr.py
+++ sms_wsj/train_baseline_asr.py
@@ -1,21 +1,54 @@
 import sacred
 import os
 
-from sms_wsj.kaldi.utils import create_data_dir
+from pathlib import Path
+from sms_wsj.utils.process_caller import run_process
 
-kaldi_root = os.environ['KALDI_ROOT']
 
+from sms_wsj.kaldi.utils import create_data_dir, create_kaldi_dir
+from paderbox.database import JsonDatabase
+
+kaldi_root = Path(os.environ['KALDI_ROOT'])
+assert kaldi_root.exists(), (
+    f'The environmental variable KALDI_ROOT has to be set to a working kaldi'
+    f' root, at the moment it points to f{kaldi_root}'
+)
+assert (kaldi_root / 'src').exists(), (
+    f'The environmental variable KALDI_ROOT has to be set to a working kaldi'
+    f' root, at the moment it points to f{kaldi_root}'
+)
+assert (kaldi_root / 'src' / 'base' / '.depend.mk').exists(), (
+    'The kaldi your KALDI_ROOT points to is not installed, please refer to'
+    ' kaldi for further information on how to install it'
+)
 ex = sacred.Experiment('Kaldi ASR baseline training')
 
 @ex.config
 def config():
-    storage_dir = None
+    egs_path = None
     json_path = None
     num_jobs = os.cpu_count()
-    assert storage_dir is not None, 'The directory where all asr training related data is stored has to be defined, use "with storage_dir=/path/to/storage/dir"'
+    stage = 0
+    # ToDo: change to kaldi_root/egs/ if no egs_path is defined?
+    assert egs_path is not None, 'The directory where all asr training related data is stored has to be defined, use "with storage_dir=/path/to/storage/dir"'
     assert json_path is not None, 'The path to the json describing the SMS-WSJ database has to be defined, use "with json_path=/path/to/json/sms_wsj.json" (for creating the json use ...)'
 
 
 @ex.automain
-def run(_config, storage_dir, json_path):
-    create_data_dir(storage_dir, json_path, db='wsj_8k')
+def run(_config, egs_path, json_path, stage):
+    sms_db = JsonDatabase(json_path)
+    sms_kaldi_dir = Path(egs_path).resolve().expanduser() / 'sms_wsj' / 's5'
+    if stage <= 0:
+        create_kaldi_dir(sms_kaldi_dir)
+    if stage <= 1:
+        create_data_dir(sms_kaldi_dir, sms_db, db_name='wsj_db')
+    run_process([
+        f'{sms_kaldi_dir}/get_model.bash',
+        '--dest_dir', f'{sms_kaldi_dir}',
+        '--train_set', str(sms_db.datasets_train[0]),
+        '--dev_sets', str(sms_db.datasets_eval[0]),
+        '--num_jobs', str(_config["num_jobs"])],
+        cwd=str(sms_kaldi_dir),
+        stdout=None, stderr=None
+    )
+
