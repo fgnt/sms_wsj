b2e4fcdaa1463417d974eeb25faf02395c3fc1b6
jensheit <heitkaemper@nt.upb.de>
2019-08-14 10:18:14 +0200

    adds function which creates json for written wsj_bss files

--- sms_wsj/database/write_files.py
+++ sms_wsj/database/write_files.py
@@ -19,49 +19,20 @@ from paderbox.database.helper import click_convert_to_path
 from paderbox.database.helper import  dump_database_as_json
 from paderbox.database.iterator import AudioReader
 from paderbox.database.wsj_bss.database import scenario_map_fn
-from paderbox.utils.mpi import RANK, SIZE, IS_MASTER
-from tqdm import tqdm
+import dlp_mpi
 
 type_mapper = {K.SPEECH_SOURCE: 'clean',
                K.SPEECH_REVERBERATION_DIRECT: 'early',
                K.SPEECH_REVERBERATION_TAIL: 'tail',
                K.NOISE_IMAGE: 'noise',
                K.OBSERVATION: 'observation'}
-appendix_mapper = {K.SPEECH_SOURCE: ['_0', '_1'],
-                   K.SPEECH_REVERBERATION_DIRECT: ['_0', '_1'],
-                   K.SPEECH_REVERBERATION_TAIL: ['_0', '_1'],
-                   K.NOISE_IMAGE: [''],
-                   K.OBSERVATION: ['']}
+
 snr_range = (20, 30)
 
 
-def normalize_audio(d: dict):
-    """
-    Convert all arrays in the dict to np.float32.
-    To have a low quantisation error, tha maximum of all audios is calculated
-    and used as normalisation.
-
-    >>> np.float64(np.finfo(np.float32).max)
-    32767.0
-    >>> normalize_audio({'a': [2., 3.], 'b': [4., 5.]})
-    {'a': array([13106, 19660], dtype=int16), 'b': array([26213, 32767], dtype=int16)}
-
-    """
-
-    dtype = np.float32
-    float32_max = np.float64(np.finfo(dtype).max)
-
-    correction = float32_max / np.max([np.max(np.abs(v)) for v in d.values()])
-
-    return {
-        k: np.array(correction * np.array(v), dtype)
-        for k, v in d.items()
-    }
-
-
-def write_wavs(dst_dir, ds, write_all=False):
+def write_wavs(dst_dir, db, write_all=False):
     if write_all:
-        if IS_MASTER:
+        if mpi.IS_MASTER:
             [(dst_dir / data_type).mkdir(exist_ok=False)
              for data_type in type_mapper.values()]
         map_fn = partial(
@@ -71,7 +42,7 @@ def write_wavs(dst_dir, ds, write_all=False):
             add_speech_reverberation_tail=True
         )
     else:
-        if IS_MASTER:
+        if dlp_mpi.IS_MASTER:
             (dst_dir / 'observation').mkdir(exist_ok=False)
         map_fn = partial(
             scenario_map_fn, snr_range=snr_range,
@@ -79,9 +50,10 @@ def write_wavs(dst_dir, ds, write_all=False):
             add_speech_reverberation_direct=False,
             add_speech_reverberation_tail=False
         )
-    ds = ds.map(AudioReader(audio_keys=[K.RIR, K.SPEECH_SOURCE])).map(map_fn)
+    ds = db.get_dataset(['train_si284', 'cv_dev93', 'test_eval92']).map(
+        AudioReader(audio_keys=[K.RIR, K.SPEECH_SOURCE])).map(map_fn)
 
-    for example in tqdm(ds[RANK::SIZE], disable=not IS_MASTER):
+    for example in dlp_mpi.split_managed(ds):
         audio_dict = example[K.AUDIO_DATA]
         example_id = example[K.EXAMPLE_ID]
         del audio_dict[K.SPEECH_IMAGE]
@@ -89,20 +61,22 @@ def write_wavs(dst_dir, ds, write_all=False):
         if not write_all:
             del audio_dict[K.SPEECH_SOURCE]
             del audio_dict[K.NOISE_IMAGE]
-        audio_dict = normalize_audio(audio_dict)
+        assert all([np.max(np.abs(v)) <= 1 for v in audio_dict.values()])
         for key, value in audio_dict.items():
             path = dst_dir / type_mapper[key]
             if key in [K.OBSERVATION, K.NOISE_IMAGE]:
                 value = value[None]
             for idx, signal in enumerate(value):
-                filename = example_id + appendix_mapper[key][idx] + '.wav'
+                appendix = f'_{idx}' if len(value) > 1 else ''
+                filename = example_id + appendix + '.wav'
                 audio_path = str(path / filename)
                 with soundfile.SoundFile(
                         audio_path, subtype='FLOAT', mode='w', samplerate=8000,
                         channels=1 if signal.ndim == 1 else signal.shape[0]
-                ) as f:
-                    f.write(signal.T)
-    if IS_MASTER:
+                ) as f: f.write(signal.T)
+
+    dlp_mpi.barrier()
+    if dlp_mpi.IS_MASTER:
         created_files = list(dst_dir.rglob("*.wav"))
         logging.info(f"Written {len(created_files)} wav files.")
         if write_all:
@@ -112,23 +86,29 @@ def write_wavs(dst_dir, ds, write_all=False):
             assert len(created_files) == len(ds), len(created_files)
 
 
-def create_json(dst_dir, ds, write_all):
-    json_dict = dict()
-
-    for ex in ds:
-        ex_id = ex[K.EXAMPLE_ID]
-        if write_all:
-            ex[K.AUDIO_PATH].update({
-                key: dst_dir / data_type / (ex_id + appendix + '.wav')
-                for key, data_type in type_mapper
-                for appendix in appendix_mapper[key]
-            })
-        else:
-            ex[K.AUDIO_PATH].update({
-                'observation': dst_dir / 'observation' / (ex_id + '.wav')
-            })
-        del ex[ex_id]
-        json_dict[ex_id] = ex
+def create_json(dst_dir, db, write_all):
+    json_dict = dict(datasets=dict())
+    database_dict = db.database_dict['datasets']
+    for dataset_name, dataset in database_dict.items():
+        dataset_dict = dict()
+        for ex_id, ex in dataset.items():
+            if write_all:
+                for key, data_type in type_mapper.items():
+                    if key in ['observation', 'noise_image']:
+                        ex[K.AUDIO_PATH][key] = [
+                            dst_dir / data_type / (ex_id + '.wav'),
+                         ]
+                    else:
+                        ex[K.AUDIO_PATH][key] = [
+                            dst_dir / data_type / (ex_id + '_0.wav'),
+                            dst_dir / data_type / (ex_id + '_1.wav')
+                        ]
+            else:
+                ex[K.AUDIO_PATH].update({
+                    'observation': dst_dir / 'observation' / (ex_id + '.wav')
+                })
+            dataset_dict[ex_id] = ex
+            json_dict['datasets'][dataset_name] = dataset_dict
     return json_dict
 
 
@@ -172,14 +152,23 @@ if __name__ == '__main__':
         assert json_path.is_file(), json_path
 
         db = JsonDatabase(json_path)
-        ds = db.get_dataset(['train_si284', 'cv_dev93', 'test_eval92'])
-        write_wavs(dst_dir, json_path, write_all=write_all)
+        if not any([(dst_dir / data_type).exists() for data_type in type_mapper.keys()]):
+            write_wavs(dst_dir, db, write_all=write_all)
+        else:
+            num_wav_files = len(list(dst_dir.rglob("*.wav")))
+            if write_all and  num_wav_files == (3 * 2 + 2) * 32000:
+                print('Wav files seem to exist. They are not overwritten.')
+            elif not write_all and num_wav_files == 32000:
+                print('Wav files seem to exist. They are not overwritten.')
+            else:
+                raise ValueError(
+                    'Not all wav files exist. However, the directory structure'
+                    ' already exists.')
 
-        if overwrite_json and IS_MASTER:
+        if dlp_mpi.IS_MASTER and overwrite_json:
             print(f'Creating a new json and saving it to {json_path}')
-            new_json = create_json(dst_dir, ds, write_all)
-            new_json[K.DATASETS] = db.database_dict[K.DATASETS]
-            dump_database_as_json(json_path, new_json)
+            updated_json = create_json(dst_dir, db, write_all)
+            dump_database_as_json(json_path, updated_json)
 
         logging.info(f"Done - {time.ctime()}")
 
