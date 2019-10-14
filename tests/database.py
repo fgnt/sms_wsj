ac30da32ba48006cac52a24fafbae4a5f5b9ed94
Christoph <cbj@mail.uni-paderborn.de>
2019-10-14 18:56:37 +0200

    update database test for recent changes

--- tests/database.py
+++ tests/database.py
@@ -1,8 +1,10 @@
 import numpy as np
 from sms_wsj.database.database import SmsWsj, AudioReader
 
+from paderbox.database.wsj_bss import WsjBss, scenario_map_fn
 
-def test_example():
+
+def test_first_example():
     db = SmsWsj()
     ds = db.get_dataset('cv_dev93')
     ds = ds.map(AudioReader(rir=True))
@@ -20,13 +22,42 @@ def test_example():
     )
 
     assert len(example['audio_data']) == 7
-    assert example['audio_data']['observation'].shape == (6, 103650)
-    assert example['audio_data']['noise_image'].shape == (6, 103650)
-    assert example['audio_data']['speech_reverberation_early'].shape == (2, 6, 103650)
-    assert example['audio_data']['speech_reverberation_tail'].shape == (2, 6, 103650)
-    assert example['audio_data']['speech_source'].shape == (2, 103650)
-    assert example['audio_data']['speech_image'].shape == (2, 6, 103650)
+    assert example['audio_data']['observation'].shape == (6, 93389)
+    assert example['audio_data']['noise_image'].shape == (6, 93389)
+    assert example['audio_data']['speech_reverberation_early'].shape == (2, 6, 93389)
+    assert example['audio_data']['speech_reverberation_tail'].shape == (2, 6, 93389)
+    assert example['audio_data']['speech_source'].shape == (2, 93389)
+    assert example['audio_data']['speech_image'].shape == (2, 6, 93389)
     assert example['audio_data']['rir'].shape == (2, 6, 8192)
+
+    assert list(example.keys()) == [
+        'room_dimensions', 'sound_decay_time', 'source_position',
+        'sensor_position', 'example_id', 'num_speakers', 'speaker_id',
+        'source_id', 'gender', 'kaldi_transcription', 'log_weights',
+        'num_samples', 'offset', 'audio_path', 'snr', 'dataset', 'audio_data'
+    ]
+
+    assert example['example_id'] == '0_4k6c0303_4k4c0319'
+    assert example['snr'] == 23.287502642941252
+    assert example['room_dimensions'] == [[8.169], [5.905], [3.073]]
+    assert example['source_position'] == [[3.312, 3.0], [1.921, 2.379], [1.557, 1.557]]
+    assert example['sensor_position'] == [
+        [4.015, 3.973, 4.03, 4.129, 4.172, 4.115],
+        [3.265, 3.175, 3.093, 3.102, 3.192, 3.274],
+        [1.55, 1.556, 1.563, 1.563, 1.558, 1.551]]
+    assert example['sound_decay_time'] == 0.387
+    assert example['offset'] == [52476, 0]
+    assert example['log_weights'] == [0.9885484337248203, -0.9885484337248203]
+    assert example['num_samples'] == {'observation': 93389,
+                                      'speech_source': [31633, 93389]}
+
+
+def test_random_example():
+    db = SmsWsj()
+    ds = db.get_dataset('cv_dev93')
+    ds = ds.map(AudioReader(rir=True))
+
+    example = ds.random_choice()
 
     np.testing.assert_allclose(
         example['audio_data']['observation'],
@@ -38,37 +69,18 @@ def test_example():
         atol=1e-7
     )
 
-    assert list(example.keys()) == [
-        'room_dimensions', 'sound_decay_time', 'source_position',
-        'sensor_position', 'num_speakers', 'speaker_id', 'source_id',
-        'gender', 'kaldi_transcription', 'log_weights', 'num_samples',
-        'offset', 'audio_path', 'snr', 'example_id', 'dataset', 'audio_data']
-
-    assert example['example_id'] == '4k0c0301_4k6c030t_0'
-    assert example['snr'] == 29.749852569493584
-    assert example['room_dimensions'] == [[8.169], [5.905], [3.073]]
-    assert example['source_position'] == [[2.443, 2.71], [3.104, 2.135], [1.557, 1.557]]
-    assert example['sensor_position'] == [[3.899, 3.8, 3.759, 3.817, 3.916, 3.957],
-                        [3.199, 3.189, 3.098, 3.017, 3.027, 3.118],
-                        [1.413, 1.418, 1.423, 1.423, 1.417, 1.413]]
-    assert example['sound_decay_time'] == 0.354
-    assert example['offset'] == [0, 17423]
-    assert example['log_weights'] == [1.2027951449295022, -1.2027951449295022]
-    assert example['num_samples'] == {'observation': 103650,
-                                      'speech_source': [103650, 56335]}
-
 
 def test_order():
     db = SmsWsj()
 
     ds = db.get_dataset('cv_dev93')
     for scenario_id, example in enumerate(ds):
-        assert scenario_id == int(example['example_id'].split('_')[-1])
+        assert scenario_id == int(example['example_id'].split('_')[0])
 
     ds = db.get_dataset('test_eval92')
     for scenario_id, example in enumerate(ds):
-        assert scenario_id == int(example['example_id'].split('_')[-1])
+        assert scenario_id == int(example['example_id'].split('_')[0])
 
     ds = db.get_dataset('train_si284')
     for scenario_id, example in enumerate(ds):
-        assert scenario_id == int(example['example_id'].split('_')[-1])
+        assert scenario_id == int(example['example_id'].split('_')[0])
