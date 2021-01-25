c967573bdacf2649866a23e21239695b0f36168f
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2021-01-25 10:26:09 +0100

    Revert "fix doctest"
    This reverts commit b424ac62df114f741277091230f5b9bbf4581d36.

--- sms_wsj/database/database.py
+++ sms_wsj/database/database.py
@@ -13,22 +13,22 @@ class SmsWsj(lazy_dataset.database.JsonDatabase):
     >>> db.get_dataset()
     Traceback (most recent call last):
     ...
-    TypeError: Missing dataset_name, use e.g.: ('train_si284', 'cv_dev93', 'test_eval92')
+    TypeError: Missing dataset_name, use e.g.: ('cv_dev93', 'test_eval92', 'train_si284')
     >>> db.get_dataset('train_si284')
-      DictDataset(name='train_si284', len=33561)
+      DictDataset(len=30000)
     MapDataset(_pickle.loads)
     >>> db.get_dataset('cv_dev93')
-      DictDataset(name='cv_dev93', len=982)
+      DictDataset(len=500)
     MapDataset(_pickle.loads)
     >>> db.get_dataset('test_eval92')
-      DictDataset(name='test_eval92', len=1332)
+      DictDataset(len=1500)
     MapDataset(_pickle.loads)
     >>> db.get_dataset(['train_si284', 'cv_dev93', 'test_eval92'])
-        DictDataset(name='train_si284', len=33561)
+        DictDataset(len=30000)
       MapDataset(_pickle.loads)
-        DictDataset(name='cv_dev93', len=982)
+        DictDataset(len=500)
       MapDataset(_pickle.loads)
-        DictDataset(name='test_eval92', len=1332)
+        DictDataset(len=1500)
       MapDataset(_pickle.loads)
     ConcatenateDataset()
     >>> ds = db.get_dataset('cv_dev93')
@@ -36,26 +36,31 @@ class SmsWsj(lazy_dataset.database.JsonDatabase):
     {'audio_path': {'noise_image': ...,
                     'observation': ...,
                     'rir': [...,...],
-                    'speech_reverberation_early': [...,...],
+                    'speech_reverberation_direct': [...,...],
                     'speech_reverberation_tail': [...,...],
                     'speech_source': [...,...]},
      'dataset': 'cv_dev93',
-     'example_id': '0_4k6c0303_4k4c0319',
-     'gender': ['male', 'female'],
-     'kaldi_transcription': [...,...],
+     'example_id': '4k0c0301_4k6c030t_0',
+     'gender': ['male', 'male'],
+     'kaldi_transcription': ['SAATCHI OFFICIALS SAID THE MANAGEMENT '
+                             'RE:STRUCTURING MIGHT ACCELERATE ITS EFFORTS TO '
+                             'PERSUADE CLIENTS TO USE THE FIRM AS A ONE STOP '
+                             'SHOP FOR BUSINESS SERVICES',
+                             'THEY HAVE SPENT SEVEN YEARS AND MORE THAN THREE '
+                             'HUNDRED MILLION DOLLARS IN U. S. AID BUILDING '
+                             "THE AREA'S BIGGEST INSURGENT FORCE"],
      'log_weights': [1.2027951449295022, -1.2027951449295022],
-     'num_samples': {'observation': 93389, 'original_source': [31633, 93389]},
+     'num_samples': {'observation': 103650, 'speech_source': [103650, 56335]},
      'num_speakers': 2,
-     'offset': [52476, 0],
+     'offset': [0, 17423],
      'room_dimensions': [[8.169], [5.905], [3.073]],
-     'sensor_position': [[4.015, 3.973, 4.03, 4.129, 4.172, 4.115],
-                         [3.265, 3.175, 3.093, 3.102, 3.192, 3.274],
-                         [1.55, 1.556, 1.563, 1.563, 1.558, 1.551]],
-     'snr': 23.287502642941252,
-     'sound_decay_time': 0.387,
-     'source_id': ['4k6c0303', '4k4c0319'],
-     'source_position': [[3.312, 3.0], [1.921, 2.379], [1.557, 1.557]],
-     'speaker_id': ['4k6', '4k4']}
+     'sensor_position': [[3.899, 3.8, 3.759, 3.817, 3.916, 3.957],
+                         [3.199, 3.189, 3.098, 3.017, 3.027, 3.118],
+                         [1.413, 1.418, 1.423, 1.423, 1.417, 1.413]],
+     'sound_decay_time': 0.354,
+     'source_id': ['4k0c0301', '4k6c030t'],
+     'source_position': [[2.443, 2.71], [3.104, 2.135], [1.557, 1.557]],
+     'speaker_id': ['4k0', '4k6']}
     """
 
     @classmethod
@@ -76,12 +81,7 @@ class SmsWsj(lazy_dataset.database.JsonDatabase):
                 f'\t>>> `{name}()`'
             ) from e
 
-    def __init__(
-            self,
-            json_path='/scratch/hpc-prf-nt1/cbj/sms_wsj/sms_wsj.json',
-            # json_path='/scratch/hpc-prf-nt1/cbj/tmp/sms_wsj/sms_wsj.json',
-            # json_path='/scratch/hpc-prf-nt1/jensheit/jsons/final_sms_wsj.json',
-    ):
+    def __init__(self, json_path=None):
         if json_path is None:
             json_path = self.default_json_path()
 
@@ -163,12 +163,7 @@ class AudioReader:
         import soundfile
 
         if isinstance(file, (tuple, list)):
-            data = [cls._rec_audio_read(f) for f in file]
-            data_np = np.array(data)
-            if data_np.dtype == np.object:
-                return data
-            else:
-                return data_np
+            return np.array([cls._rec_audio_read(f) for f in file])
         elif isinstance(file, (dict)):
             return {k: cls._rec_audio_read(v) for k, v in file.items()}
         else:
